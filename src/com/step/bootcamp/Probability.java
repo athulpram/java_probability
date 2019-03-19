dccc07e23c40a2e057147171100251937ee47710
praathul <44019979+athulpram@users.noreply.github.com>
2019-03-19 10:20:27 +0530

    Represent the probability using Probability object
    Throw a InvalidProbabilityException when the probability is invalid

---
+++ src/com/step/bootcamp/Probability.java
@@ -0,0 +1,9 @@
+package com.step.bootcamp;
+
+public class Probablity {
+    private double value;
+
+    public Probablity(double value) {
+        this.value = value;
+    }
+}
---
+++ test/com/step/bootcamp/ProbablityTest.java
@@ -0,0 +1,7 @@
+package com.step.bootcamp;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProbablityTest {
+
+}
